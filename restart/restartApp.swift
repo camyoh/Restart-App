c0404f11fd6381d78170119b1addcda48241b842
Andres <carlos.camyoh@gmail.com>
2023-02-12 14:00:33 -0500

    Initial Commit

---
+++ restart/restartApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartApp.swift
+//  restart
+//
+//  Created by Andres Mendieta on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct restartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
