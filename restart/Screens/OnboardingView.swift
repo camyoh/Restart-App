d7529bcb99a98db2444e7993e33eb0e047a08653
Andres <carlos.camyoh@gmail.com>
2023-02-12 15:55:36 -0500

    ident

--- restart/Screens/OnboardingView.swift
+++ restart/Screens/OnboardingView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+  static var previews: some View {
+    OnboardingView()
+  }
 }
--- restart/restartApp.swift
+++ restart/restartApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct restartApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
