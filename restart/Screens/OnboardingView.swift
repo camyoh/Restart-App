d6dd0734c79aaedfd66e25ff06f8d93553011b66
Andres <carlos.camyoh@gmail.com>
2023-02-15 19:13:02 -0500

    add the text in the scroll button

--- restart/Screens/OnboardingView.swift
+++ restart/Screens/OnboardingView.swift
@@ -76,6 +76,13 @@ struct OnboardingView: View {
             .padding(8)
           
           // 2. CALL-TO-ACTION (STATIC)
+          
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+          
           // 3. CAPSULE (DYNAMIC WIDTH)
           
           HStack {
