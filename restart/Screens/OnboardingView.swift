88a7e40af4f0192e525dcd8249d4404778c85553
Andres <carlos.camyoh@gmail.com>
2023-02-15 20:04:10 -0500

    ajustando el ancho del fondo del boton para que parezca que crece con el boton

--- restart/Screens/OnboardingView.swift
+++ restart/Screens/OnboardingView.swift
@@ -84,7 +84,7 @@ struct OnboardingView: View {
           HStack {
             Capsule()
               .fill(Color("ColorRed"))
-              .frame(width: 80)
+              .frame(width: buttonOffset + 80)
             
             Spacer()
           }
